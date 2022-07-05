f032253605ecc030c0aed49a8e68f68ca0ea4d44
Alena <alesikxxx@mail.ru>
2022-07-06 00:02:13 +0300

    Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"Большее число {a} , меньшее число {b}");
+}
+else if (b > a)
+{
+    Console.WriteLine($"Большее число {b} , меньшее число {a}");
+}
+
+
+
